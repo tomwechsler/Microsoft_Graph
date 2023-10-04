cf1a89a7f191c9d8b82c6cb9e5676c1aca3eaa99
Tom Wechsler <tom.wechsler@outlook.com>
2023-10-04 15:38:31 +0200

    lines edited

--- Microsoft_365/Office_365_Management_API/Investigate_logs.ps1
+++ Microsoft_365/Office_365_Management_API/Investigate_logs.ps1
@@ -32,6 +32,10 @@ Invoke-WebRequest -Method Post -Headers $headerParams -Uri "$resource/api/v1.0/$
 
 Invoke-WebRequest -Method Post -Headers $headerParams -Uri "$resource/api/v1.0/$tenantGUID/activity/feed/subscriptions/start?contentType=Audit.Exchange"
 
+##########
+#EXAMPLE 1
+##########
+
 #Retrieve Items from Audit.AzurectiveDirectory
 Invoke-WebRequest -Method GET -Headers $headerParams -Uri "$resource/api/v1.0/$tenantGUID/activity/feed/subscriptions/content?contentType=Audit.AzureActiveDirectory"
 
@@ -82,3 +86,59 @@ foreach ($contentItem in $contentResult) {
     }
 }
 $table | Format-Table
+
+##########
+#EXAMPLE 2
+##########
+
+# Replace these values with your own
+$ClientID = "<Client ID>"
+$ClientSecret = "<Client Secret>"
+$tenantdomain = "<Domain>.onmicrosoft.com"
+$TenantGUID = "<Tenant ID>"
+$loginURL = "https://login.microsoftonline.com/"
+$resource = "https://manage.office.com"
+
+$body = @{grant_type="client_credentials";resource=$resource;client_id=$ClientID;client_secret=$ClientSecret}
+$oauth = Invoke-RestMethod -Method Post -Uri $loginURL/$tenantdomain/oauth2/token?api-version=1.0 -Body $body
+$headerParams = @{'Authorization'="$($oauth.token_type) $($oauth.access_token)"}
+
+#Retrieve Items from Audit.SharePoint
+Invoke-WebRequest -Method GET -Headers $headerParams -Uri "$resource/api/v1.0/$tenantGUID/activity/feed/subscriptions/content?contentType=Audit.General"
+
+#Perform a Date Range Query
+$startTimeDt = "2023-10-03T05:00:00.000Z"
+$endTimeDt = "2023-10-03T21:00:10.123Z"
+
+$response = Invoke-WebRequest -Method GET -Headers $headerParams -Uri "$resource/api/v1.0/$tenantGUID/activity/feed/subscriptions/content?startTime=$startTimeDt&endTime=$endTimeDt&contentType=Audit.General&PublisherIdentifier=$TenantGUID"
+
+#View Response
+$response | Select-Object -ExpandProperty Content
+
+#Loop All Items and View Actual Records
+$contentResult = $response.Content | ConvertFrom-Json
+foreach ($contentItem in $contentResult) {
+    $blobUri = $contentItem.contentUri
+    try {
+        $blobResponse = Invoke-WebRequest -Method Get -Headers $headerParams -Uri $blobUri
+        $results = $blobResponse.Content | ConvertFrom-Json
+        Write-Output $results
+    }
+    catch {
+        Write-Error $_.Exception.Message
+    }
+}
+
+#List specific properties
+$contentResult = $response.Content | ConvertFrom-Json
+foreach ($contentItem in $contentResult) {
+    $blobUri = $contentItem.contentUri
+    try {
+        $blobResponse = Invoke-WebRequest -Method Get -Headers $headerParams -Uri $blobUri
+        $results = $blobResponse.Content | ConvertFrom-Json | Select-Object CreationTime, Workload, ResultStatus
+        $results | Format-Table
+    }
+    catch {
+        Write-Error $_.Exception.Message
+    }
+}
