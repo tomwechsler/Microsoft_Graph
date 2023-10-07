3282a17de29c42c5ca8264b89773cfcbe0d20189
Tom Wechsler <tom.wechsler@outlook.com>
2023-10-07 13:54:15 +0200

    new files

--- Intune/Connect_with_app_certificate.ps1
+++ Intune/Connect_with_app_certificate.ps1
@@ -2,15 +2,17 @@
 
 $certName = 'IntuneGraphAppCert'
 
-$cert = New-SelfSignedCertificate -Subject "CN=$certName" -CertStoreLocation "Cert:\CurrentUser\My" -KeyExportPolicy Exportable -KeySpec Signature -KeyLength 2048 -KeyAlgorithm RSA -HashAlgorithm SHA256 -NotAfter (get-date).AddYears(5)
+$cert = New-SelfSignedCertificate -Subject "CN=$certName" -CertStoreLocation "Cert:\CurrentUser\My" -KeyExportPolicy Exportable -KeySpec Signature -KeyLength 2048 -KeyAlgorithm RSA -HashAlgorithm SHA256 -NotAfter (get-date).AddYears(1)
 
 Export-Certificate -Cert $cert -FilePath "C:\certs\$certName.cer"
 
-$TenantId = '6e58ed26-175b-4b81-ae01-d9f865953b07'
-$AppId = '85a1ea9f-28d7-450a-be96-8f1e788c7396'
+$TenantId = '77e01716-a6a2-4f99-b864-xxxxxxxxxxxx'
+$AppId = '5c14b994-2290-4f84-9069-xxxxxxxxxxxx'
 $certName = 'IntuneGraphAppCert'
 
 $Cert = Get-ChildItem -Path 'Cert:\CurrentUser\My' | Where-Object { $_.Subject -eq "CN=$CertName" }
 
 Connect-MgGraph -TenantId $TenantId -ClientId $AppId -Certificate $Cert 
 
+#We check the permissions
+(Get-MgContext).Scopes
